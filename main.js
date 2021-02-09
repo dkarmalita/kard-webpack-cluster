c764b205d50db60c38a6a01400e8c5d9d0600931
Dmitriy Karmalita <dmitriy.ka@maxitechs.com>
2021-02-09 14:50:37 +0200

    [Update] main.js]

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ const spawnSync = require('./lib/spawn-sync');
 const builderPath = path.join(__dirname, './lib/builder.js')
 console.log(builderPath)
 
-// Set id variable with the running mode ('development', 'production', 'debugging')
 // WARNING: It must be set& Othercase, the cluster builder reject its run
+// NOTE: (kard) We need to convert context and args to the process config here.
+// Also, we allow to prepare the config on the uppe level for usage in 3d paty components.
+const config = {
+  wpConfigPath: path.join(__dirname, './webpack/dev.config.js'),
+  builderConfigPath: path.join(process.cwd(), './brands/build.config.js'),
+  optionsField: 'brands', // of builderConfigPath
+}
+
 if(!process.env.__WP_BUILDER_CONFIG__){
-  process.env.__WP_BUILDER_CONFIG__ = JSON.stringify({})
+  process.env.__WP_BUILDER_CONFIG__ = JSON.stringify(config)
 }
 
 // Run cluster builder
